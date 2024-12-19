c11d68146b97147cc4fc71a58d232a5c8c4341d0
amirhamjacse <amirhamza824@gmail.com>
2024-12-19 12:55:25 +0600

    added products view

--- products/views/product.py
+++ products/views/product.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from products.models import Product
+from rest_framework.views import APIView
+from products.serializer import ProductSerializer
+from rest_framework.response import Response
 
 
-class ProductView():
-    pass
+class ProductView(APIView):
+    def get(self, request):
+        product_obj = Product.objects.all()
+        serializer = ProductSerializer(product_obj, many=True)
+        return Response(serializer.data)
